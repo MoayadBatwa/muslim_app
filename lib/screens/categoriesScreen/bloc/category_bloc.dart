32d7bc1cb967cd6ecf22d2448c4c1d380cf542e4
Moayad Batwa <123902670+MoayadBatwa@users.noreply.github.com>
2026-03-04 23:02:51 +0300

    set the main screen

---
+++ lib/screens/categoriesScreen/bloc/category_bloc.dart
@@ -0,0 +1,14 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'category_event.dart';
+part 'category_state.dart';
+
+class CategoryBloc extends Bloc<CategoryEvent, CategoryState> {
+  CategoryBloc() : super(CategoryInitial()) {
+    on<CategoryEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
